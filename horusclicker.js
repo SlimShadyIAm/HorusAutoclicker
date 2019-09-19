9d208ad8c2e15c2491beb7fd8fab9765069e3e5b
Aamir Farooq <aamir@farooq.xyz>
2019-09-19 14:42:54 +0200

    Add all files

---
+++ horusclicker.js
@@ -0,0 +1,25 @@
+// this border shows that the extension is active.
+document.body.style.border = "5px solid red";
+
+function checkForCards() {
+	// select all card elements on the page
+	var cards = document.getElementsByClassName("card");
+
+	// loop through the cards to see if we can find list for today
+	for (i = 0; i < cards.length; i++) {
+		var cardTitle = cards[i].children[0].children[0].children[0].title;
+		if (cardTitle == "Sign-off: CAO Today") {
+			var joinQueueButton =
+				cards[i].children[0].children[0].children[2].children[0];
+			if (joinQueueButton.title == "Join queue") {
+				// are we already in the queue?
+				joinQueueButton.click(); // no, so join the queue :)
+			}
+		}
+	}
+}
+
+// run the check for new queues every 50 milliseconds
+setInterval(() => {
+	checkForCards();
+}, 50);
